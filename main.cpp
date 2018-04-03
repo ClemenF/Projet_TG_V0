b1fc8e0925855c33e643eab9e70bb0b35e0e899d
ClemenF <clemen.fayol@gmail.com>
2018-04-03 13:39:42 +0200

    ajout

--- main.cpp
+++ main.cpp
@@ -6,7 +6,13 @@ int main()
     std::cout << "-------------  D�but du projet  -----------" << std::endl;
     std::cout << " ---- test de modif ---- " << std::endl;
     int a = 0;
-    
+    a = a + 3;
+    std::cout << std::endl;
+
+    if (a==3) {
+      std::cout << " a = 3 " ;
+    }
+
     return 0;
 }
 END_OF_MAIN();
